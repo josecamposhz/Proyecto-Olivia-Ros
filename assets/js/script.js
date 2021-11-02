dc76d8fc582b4ebdd09dc7d95c66c9eabc2977c9
Jose Campos <josecamposh95@gmail.com>
2021-11-02 13:56:40 -0300

    styles with boostrap 5

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,26 @@
-$(function(){
+// $(function(){
 
-  $("a").click(function(event){
-    if (this.hash !== "") {
-      event.preventDefault();
+//   $("a").click(function(event){
+//     if (this.hash !== "") {
+//       event.preventDefault();
 
-      var gato = this.hash;
+//       var gato = this.hash;
 
-      $("html, body").animate({
-        scrollTop: $(gato).offset().top
-      }, 800, function(){
-        window.location.hash = gato;
-      });
-    }
-  });
+//       $("html, body").animate({
+//         scrollTop: $(gato).offset().top
+//       }, 800, function(){
+//         window.location.hash = gato;
+//       });
+//     }
+//   });
 
-  $('[data-toggle="popover"]').popover();
+//   $('[data-toggle="popover"]').popover();
 
+// });
+
+var popoverTriggerList = [].slice.call(
+	document.querySelectorAll('[data-bs-toggle="popover"]')
+);
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+	return new bootstrap.Popover(popoverTriggerEl);
 });
